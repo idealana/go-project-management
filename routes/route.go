63adb86cd61184a42698613468e233e43651e945
idealana <lanarf97@gmail.com>
2026-07-12 21:48:56 +0700

    feat: add jwt middleware in routes

--- routes/route.go
+++ routes/route.go
@@ -3,9 +3,12 @@ package routes
 import (
 	"log"
 
+	"github.com/idealana/go-project-management/config"
 	"github.com/idealana/go-project-management/controllers"
+	"github.com/idealana/go-project-management/utils"
 
 	"github.com/gofiber/fiber/v2"
+	jwtWare "github.com/gofiber/jwt/v3"
 	"github.com/joho/godotenv"
 )
 
@@ -17,4 +20,12 @@ func Setup(app *fiber.App, uc *controllers.UserController) {
 
 	app.Post("/v1/auth/register", uc.Register)
 	app.Post("/v1/auth/login", uc.Login)
+
+	api := app.Group("/api/v1", jwtWare.New(jwtWare.Config{
+		SigningKey: []byte(config.AppConfig.JWTSecret),
+		ContextKey: "user",
+		ErrorHandler: func (ctx *fiber.Ctx, err error) error {
+			return utils.Unauthorized(ctx, "Unathorized", err.Error())
+		},
+	}))
 }
