519e72edaf7418b3bd0d2b241f6d3551529767f0
idealana <lanarf97@gmail.com>
2026-07-12 15:56:47 +0700

    feat: add response json utils

---
+++ utils/response.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Response struct {
+	Status string `json:"status"`
+	ResponseCode int `json:"response_code"`
+	Message string `json:"message,omitempty"`
+	Data interface{} `json:"data,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+func Success(c *fiber.Ctx, message string, data interface{}) error {
+	return c.Status(fiber.StatusOK).JSON(Response{
+		Status: "Success",
+		ResponseCode: fiber.StatusOK,
+		Message: message,
+		Data: data,
+	})
+}
+
+func Created(c *fiber.Ctx, message string, data interface{}) error {
+	return c.Status(fiber.StatusCreated).JSON(Response{
+		Status: "Created",
+		ResponseCode: fiber.StatusCreated,
+		Message: message,
+		Data: data,
+	})
+}
+
+func BadRequest(c *fiber.Ctx, message string, err string) error {
+	return c.Status(fiber.StatusBadRequest).JSON(Response{
+		Status: "Error Bad Request",
+		ResponseCode: fiber.StatusBadRequest,
+		Message: message,
+		Error: err,
+	})
+}
+
+func NotFound(c *fiber.Ctx, message string, err string) error {
+	return c.Status(fiber.StatusNotFound).JSON(Response{
+		Status: "Error Not Found",
+		ResponseCode: fiber.StatusNotFound,
+		Message: message,
+		Error: err,
+	})
+}
+
+func Unauthorized(c *fiber.Ctx, message string, err string) error {
+	return c.Status(fiber.StatusUnauthorized).JSON(Response{
+		Status: "Error Not Found",
+		ResponseCode: fiber.StatusUnauthorized,
+		Message: message,
+		Error: err,
+	})
+}
+
+func InternalServerError(c *fiber.Ctx, message string, err string) error {
+	return c.Status(fiber.StatusInternalServerError).JSON(Response{
+		Status: "Internal Server Error",
+		ResponseCode: fiber.StatusInternalServerError,
+		Message: message,
+		Error: err,
+	})
+}
