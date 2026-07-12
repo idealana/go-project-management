20e4809c06b2e537454b0f2710adafe980ee1956
idealana <lanarf97@gmail.com>
2026-07-12 16:57:52 +0700

    feat: add user controller
    add method Register() to add new user

---
+++ controllers/user_controller.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"github.com/idealana/go-project-management/models"
+	"github.com/idealana/go-project-management/services"
+	"github.com/idealana/go-project-management/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserController struct {
+	service services.UserService
+}
+
+func NewUserController(
+	s services.UserService,
+) *UserController {
+	return &UserController{
+		service: s,
+	}
+}
+
+func (c *UserController) Register(ctx *fiber.Ctx) error {
+	user := new(models.User)
+
+	if err := ctx.BodyParser(user); err != nil {
+		return utils.BadRequest(ctx, "Failed to Parsing Data", err.Error())
+	}
+
+	if err := c.service.Register(user); err != nil {
+		return utils.BadRequest(ctx, "Register Failed", err.Error())
+	}
+	
+	return utils.Success(ctx, "Register Success", user)
+}
